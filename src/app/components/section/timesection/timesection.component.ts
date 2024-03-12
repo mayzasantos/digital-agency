91c9298f78c975ef1085595d81f1d3cc83b2f3d1
Mayza <mayza.santos@outlook.com.br>
2024-03-12 17:46:50 -0300

    fix: file timesection using NgOptiomizedImage to optimize images

--- src/app/components/section/timesection/timesection.component.ts
+++ src/app/components/section/timesection/timesection.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-timesection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NgOptimizedImage],
   templateUrl: './timesection.component.html',
   styleUrl: './timesection.component.css'
 })
